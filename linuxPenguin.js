f2cf2a4617cc2058da93ff8008d7405e99170ff7
Gabe <gabe.secula@gmail.com>
2017-06-22 20:25:19 -0400

    I added just a bit for Mike's commit.

--- linuxPenguin.js
+++ linuxPenguin.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 var exec = require('child_process').exec;
 const client = new Discord.Client();
 const prefix = '$';
+var bTick = '\`\`\`';
 
 
 client.login('MzI3NDgyMTQyMzYxOTExMjk2.DC3htw.7wPGU66xGpW-nbGHynOolyvUBKw');
