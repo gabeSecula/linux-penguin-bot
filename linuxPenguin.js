93f379a60f2eda533fe7f0bbd45bb095d0590b55
Gabe <gabe.secula@gmail.com>
2017-09-25 17:12:01 -0400

    Lottery... kinda works? Not done yet.

--- linuxPenguin.js
+++ linuxPenguin.js
@@ -6,6 +6,7 @@ var lotteryChannel;
 
 client.login('MzI3NDgyMTQyMzYxOTExMjk2.DC3htw.7wPGU66xGpW-nbGHynOolyvUBKw');
 
+//This is the linux functionality
 function terminal(msg) {
   //idk what the fuck to do yet
   exec(msg.content, function(error, stdout, stderr) {
@@ -13,33 +14,43 @@ function terminal(msg) {
   });
 }
 
+//Function to set the lottery text channel
 function setLotteryChannel(msg) {
 	lotteryChannel = msg.channel;
-	msg.reply(lotteryChannel.toString());
+	msg.channel.send(lotteryChannel.toString() + " has been set as the Lottery Channel");
 }
 
+//Function to run the lottery
 function lottery(msg) {
+  /*
+    This is super primitive, a future iteration will only accept entries with a
+  prefix and will check things like multiple submissions from the same account.
+  I'm not sure if we want to make it so it's always running and checking
+  messages in that channel or if we can make it read past messages and parse
+  properly.
+  */
 	var lotteryArray = lotteryChannel.messages.array();
-	msg.reply(lotteryArray.random.toString());
-}    
+	msg.reply(lotteryArray);
+}
 
-
+//Runs when the bot successfully logs in
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setGame(`Linux Lords need no help.`);
 });
 
+//Message handler
 client.on('message', msg => {
   if ((msg.channel.type === 'dm')&&(!msg.author.bot)) {
-    terminal(msg); //this should start a session that stops doing the reply bullshit
+    terminal(msg);
   } else if (msg.content.substring(0,1) === '&') {
 	  if (msg.content === '&setLotteryChannel') {
 		  setLotteryChannel(msg);
-          } else if (msg.content === '&lottery') { 
+          } else if (msg.content === '&lottery') {
 		  lottery(msg);
 	  }
 
-	
+//This is DM functionality -- removed for the time being, still works, just trying to get the lottery together
 /* else if (msg.content.substring(0,1) === '$') {
     exec(msg.content.substring(1), function(error, stdout, stderr) {
     msg.reply('```' + stdout + stderr + ' ``` ');
