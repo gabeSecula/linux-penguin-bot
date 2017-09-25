d6f2c99903346f8e261b60b54998bce85e33b06e
gabeSecula <gabe.secula@gmail.com>
2017-09-24 22:20:18 -0400

    Added functionality for Book Club server for the time being as BookWorm is too far off right now.

--- linuxPenguin.js
+++ linuxPenguin.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 var exec = require('child_process').exec;
 const client = new Discord.Client();
 const prefix = '$';
-
+var lotteryChannel;
 
 client.login('MzI3NDgyMTQyMzYxOTExMjk2.DC3htw.7wPGU66xGpW-nbGHynOolyvUBKw');
 
@@ -13,6 +13,16 @@ function terminal(msg) {
   });
 }
 
+function setLotteryChannel(msg) {
+	lotteryChannel = msg.channel;
+	msg.reply(lotteryChannel.toString());
+}
+
+function lottery(msg) {
+	var lotteryArray = lotteryChannel.messages.array();
+	msg.reply(lotteryArray.random.toString());
+}    
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -22,9 +32,17 @@ client.on('ready', () => {
 client.on('message', msg => {
   if ((msg.channel.type === 'dm')&&(!msg.author.bot)) {
     terminal(msg); //this should start a session that stops doing the reply bullshit
-  } else if (msg.content.substring(0,1) === '$') {
+  } else if (msg.content.substring(0,1) === '&') {
+	  if (msg.content === '&setLotteryChannel') {
+		  setLotteryChannel(msg);
+          } else if (msg.content === '&lottery') { 
+		  lottery(msg);
+	  }
+
+	
+/* else if (msg.content.substring(0,1) === '$') {
     exec(msg.content.substring(1), function(error, stdout, stderr) {
     msg.reply('```' + stdout + stderr + ' ``` ');
-    });
+    });*/
   }
 });
