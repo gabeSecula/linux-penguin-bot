b1d205321be5bd0453e272f30a4df2349df24918
Gabe Secula <gabe.secula@gmail.com>
2020-03-18 22:16:31 -0400

    Cleaned up code a bit and added some TODO comments for later.

--- linuxPenguin.js
+++ linuxPenguin.js
@@ -1,28 +1,29 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-var config = require('config'); 
+const config = require('config'); //https://www.npmjs.com/package/config
 
+const client = new Discord.Client();
 var exec = require('child_process').exec;
 
 const prefix = '$';
 
 var loginToken = config.get('token'); //Put your login token in the config file.
 //https://www.npmjs.com/package/config
-//need to run: npm install config
-//Add gitignore for config
 
-client.login(loginToken); //Log into Discord *XBox ON, bring up domino's website*
 
-//This is the linux functionality
-function terminal(msg) {
-  console.log(msg); //Spawn a child process idiot
-  if(msg.content.substring(0,1) === '$') {
-    exec(msg.content.substring(1), function(error, stdout, stderr) {
+
+
+client.login(loginToken); //If you would like to use this program, replace loginToken with a discord bot token.
+
+//This is the linux functionality that processes messages
+function terminal(msg) {//TODO: Make the output code cleaner
+  console.log(msg); //TODO: Can I possibly open a limited access shell so a group could use this?
+  if (msg.content.substring(0, 1) === '$') {
+    exec(msg.content.substring(1), function (error, stdout, stderr) {
       console.log('\`\`\`' + stdout + stderr + '\`\`\`');
       msg.reply('\`\`\`' + stdout + stderr + '\`\`\`');
     });
   } else {
-    exec(msg.content, function(error, stdout, stderr) {
+    exec(msg.content, function (error, stdout, stderr) {
       console.log('\`\`\`' + stdout + stderr + '\`\`\`');
       msg.channel.send('\`\`\`' + stdout + stderr + '\`\`\`');
     });
@@ -35,18 +36,12 @@ client.on('ready', () => {
   //client.user.setGame(`You need help?`);
 });
 
+
 //Message handler
 client.on('message', msg => {
-  if ((msg.channel.type === 'dm')&&(!msg.author.bot)) {
+  if ((msg.channel.type === 'dm') && (!msg.author.bot)) { //In DMs, the message author does not need to include the '$' prefix before commands.
     terminal(msg);
-  } else if (msg.content.substring(0,1) === '&') {
-	  if (msg.content === '&setLotteryChannel') {
-		  setLotteryChannel(msg);
-          } else if (msg.content === '&lottery') {
-		  lottery(msg);
-	  }
-  }  else if (msg.content.substring(0,1) === '$'){
-      terminal(msg);
-
+  } else if (msg.content.substring(0, 1) === '$') {
+    terminal(msg);
   }
 });
