185df40487143ab6255a9666d2d764562d27f3cb
Gabe Secula <gabe.secula@gmail.com>
2020-03-20 00:44:57 -0400

    Cleaned up output code and added a helper funtion for output. Added more verbose comments accross the board. Cleaned up some misc variables and their usage. Added some TODOs for tomorrow. Project should be branched for development from here. Currently the bot runs from the same shell folder it is run from. Users do not have the power to leave said folder but it is still dangerous. This needs to be fixed. Tomorrow.

--- linuxPenguin.js
+++ linuxPenguin.js
@@ -1,47 +1,52 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); //https://discord.js.org/#/
 const config = require('config'); //https://www.npmjs.com/package/config
+const exec = require('child_process').exec; //Basic functionality in NodeJS
 
 const client = new Discord.Client();
-var exec = require('child_process').exec;
-
+const loginToken = config.get('token'); 
 const prefix = '$';
 
-var loginToken = config.get('token'); //Put your login token in the config file.
-//https://www.npmjs.com/package/config
 
 
+client.login(loginToken); //If you would like to use this program, replace loginToken with a Discord bot token.
 
-
-client.login(loginToken); //If you would like to use this program, replace loginToken with a discord bot token.
-
-//This is the linux functionality that processes messages
-function terminal(msg) {//TODO: Make the output code cleaner
-  console.log(msg); //TODO: Can I possibly open a limited access shell so a group could use this?
-  if (msg.content.substring(0, 1) === '$') {
+//This function runs messages through child_process.exec
+function shell(msg) {
+  //TODO: Make the function async so the bot can handle a bunch of messages at once.
+  //TODO: Can I possibly open a limited access shell so a group could use this all at once? Twitch Chat bot?
+  if (msg.content.substring(0, 1) === prefix) {
     exec(msg.content.substring(1), function (error, stdout, stderr) {
-      console.log('\`\`\`' + stdout + stderr + '\`\`\`');
-      msg.reply('\`\`\`' + stdout + stderr + '\`\`\`');
+       returnOutput(error, stdout, stderr, msg);
     });
   } else {
     exec(msg.content, function (error, stdout, stderr) {
-      console.log('\`\`\`' + stdout + stderr + '\`\`\`');
-      msg.channel.send('\`\`\`' + stdout + stderr + '\`\`\`');
+     returnOutput(error, stdout, stderr, msg);
     });
   }
 }
 
-//Runs when the bot successfully logs in
+//This function is here to clean up output code
+function returnOutput(error, stdout, stderr, msg) {
+  console.log('stdout: ' + stdout + '\nstderr: ' + stderr);
+  msg.reply('\`\`\`shell\n' + stdout + stderr + '\`\`\`');
+  if(error) {
+    console.log(error.stack);
+    console.log('Error code: '+ error.code);
+    console.log('Signal received: '+ error.signal);
+  }
+}
+
+//This block runs when the bot successfully logs in
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  //client.user.setGame(`You need help?`);
+  //client.user.setGame(`You need help?`); //Need to check updated version of this
 });
 
-
-//Message handler
+//This block handles message events 
 client.on('message', msg => {
-  if ((msg.channel.type === 'dm') && (!msg.author.bot)) { //In DMs, the message author does not need to include the '$' prefix before commands.
-    terminal(msg);
-  } else if (msg.content.substring(0, 1) === '$') {
-    terminal(msg);
+  if ((msg.channel.type === 'dm') && (!msg.author.bot)) { //In DMs, the message author does not need to include the prefix before commands.
+    shell(msg);
+  } else if (msg.content.substring(0, 1) === prefix) {
+    shell(msg);
   }
 });
